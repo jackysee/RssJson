11496044752c9088fe153196ced838a43d8d19f7
Gino <ginocote@gmail.com>
2017-02-19 10:27:17 -0500

    server-GC-BACK

---
+++ server-GC-BACK.js
@@ -0,0 +1,57 @@
+var http = require('http');
+var url = require('url');
+var Feed = require('./rss');
+
+var PORT = process.env.PORT || 8888;
+
+function getFeedUrl(request){
+    return url.parse(request.url, true).query.url;
+}
+
+function writeError(err, response){
+    response.writeHead(500, {
+        "Content-Type":"text/plain;charset=utf-8",
+        "Access-Control-Allow-Origin":"*"
+    });
+    response.write(err + "\n");
+    response.end();
+}
+
+function writeJsonResponse(rss, response){
+    response.writeHead(200, {
+        "Content-Type": "application/json;charset=utf-8",
+        "Access-Control-Allow-Origin":"*"
+    });
+    response.write(JSON.stringify(rss));
+    response.end();
+}
+
+function handleRequest(request, response){
+    var feedUrl = getFeedUrl(request);
+    var hasHttp = /^http:\/\/|https:\/\//.test(feedUrl);
+    if(!hasHttp){
+        feedUrl = 'http://' + feedUrl
+    }
+    var isHttps = /^https:/.test(feedUrl);
+    if(isHttps){
+        feedUrl = feedUrl.replace(/^https/, "http");
+        console.log('try connect to non-https:', feedUrl);
+    }
+    console.log('try connect to:', feedUrl);
+    Feed.load(feedUrl, function(err, rss){
+        if(err){
+            console.log('error', err);
+            writeError(err, response);
+        }
+        else{
+            writeJsonResponse(rss, response);
+        }
+    });
+}
+
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+    console.log("Sever started on %s", PORT);
+});
